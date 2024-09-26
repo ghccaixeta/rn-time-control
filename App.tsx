984a9b4e8aa495d09e889836f71851f635aad670
Gustavo Caixeta <gustavo.caixeta@gmail.com>
2024-09-26 07:43:59 -0300

    Configura tema

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
+
+import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import theme from 'src/theme';
+import { ThemeProvider } from 'styled-components/native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Inter_400Regular, Inter_700Bold});
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        {
+          fontsLoaded &&
+          <>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </>
+        }
+      </View>
+    </ThemeProvider>
   );
 }
 
