d8a69810bbdffbdf406feaecfff7c136af5cc81e
Gustavo Caixeta <gustavo.caixeta@gmail.com>
2024-09-26 08:11:42 -0300

    Adiciona `SafeAreaProvider`

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 import { Routes } from '@routes/index';
 import { useFonts } from 'expo-font';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import theme from 'src/theme';
 import { ThemeProvider } from 'styled-components/native';
 
@@ -12,7 +12,9 @@ export default function App() {
   const [fontsLoaded] = useFonts({Inter_400Regular, Inter_700Bold});
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      <SafeAreaProvider>
+        <Routes />
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './app.routes';
+import { StatusBar } from 'expo-status-bar';
 
 
 export function Routes(){
     return(
         <NavigationContainer>
             <AppRoutes />
+            <StatusBar />
         </NavigationContainer>
     );
 }
